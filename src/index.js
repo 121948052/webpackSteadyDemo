2bcd70d78615a1944037592f26abbe8e44e8352d
121948052 <121948052@qq.com>
2021-11-10 18:07:57 +0800

    add file-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+import imageFile from './pets.jpg';
 function component() {
   let element = document.createElement('div');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  // 将图像添加到我们已经存在的 div 中。
+  var myIcon = new Image();
+  myIcon.src = imageFile;
+  myIcon.width = '150';
+  myIcon.height = '100';
+
+  element.appendChild(myIcon);
   return element;
 }
 
