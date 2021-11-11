e8e04f2eb386d1285a60c6b2f50db27fde111b19
121948052 <121948052@qq.com>
2021-11-11 16:42:32 +0800

    prod mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { cube } from './math.js';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log(`Looks like we are in ${process.env.NODE_ENV} mode!`);
+}
+
 function component() {
   let element = document.createElement('pre');
   
